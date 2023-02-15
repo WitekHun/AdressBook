3294eebe9f3fa56cde27e0dc0437d3d657df53d2
Witold Hungendorfer <witekh@me.com>
2023-02-15 08:19:05 +0100

    Add subclasses and contact function

--- contacts.py
+++ contacts.py
@@ -14,7 +14,7 @@ class Contacts:
         self.label_lenght = 0
 
     def __repr__(self):
-        return f"Contacts{self}"
+        return f"{self}"
 
     def __str__(self):
         return f"({self.first_name} {self.surname}, {self.email})"
@@ -25,22 +25,77 @@ class Contacts:
 
     @label_lenght.setter
     def label_lenght(self, value):
-        if value == 0:
+        if True:
             self._label_lenght = len(self.first_name) + len(self.surname) + 1
 
 
-def create_contacts(how_many):
+class BaseContact(Contacts):
+    def __init__(self, home_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.home_phone = home_phone
+
+    def contact(self):
+        if self.home_phone[0] == "+":
+            print(
+                "Wybieram numer %s i dzwonię do %s %s"
+                % (self.home_phone, self.first_name, self.surname)
+            )
+        else:
+            print(
+                "Wybieram numer +48 %s i dzwonię do %s %s"
+                % (self.home_phone, self.first_name, self.surname)
+            )
+
+
+class BusinessContact(Contacts):
+    def __init__(self, job, company, work_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.work_phone = work_phone
+        self.company = company
+        self.job = job
+
+    def contact(self):
+        if self.work_phone[0] == "+":
+            print(
+                "Wybieram numer %s i dzwonię do %s %s"
+                % (self.work_phone, self.first_name, self.surname)
+            )
+        else:
+            print(
+                "Wybieram numer +48 %s i dzwonię do %s %s"
+                % (self.work_phone, self.first_name, self.surname)
+            )
+
+
+def create_contacts(kind, how_many):
     card_list = []
-    for i in range(0, how_many):
-        card_list.append(card_list)
-        card_list[i] = Contacts(
-            first_name=fake.first_name(),
-            surname=fake.last_name(),
-            address=fake.address(),
-            email=fake.email(),
-        )
-        # card_list[i].label_lenght = 0
-    return card_list
+    if kind == BaseContact:
+        for i in range(0, how_many):
+            card_list.append(card_list)
+            card_list[i] = BaseContact(
+                first_name=fake.first_name(),
+                surname=fake.last_name(),
+                address=fake.address(),
+                email=fake.email(),
+                home_phone=fake.phone_number(),
+            )
+        return card_list
+    elif kind == BusinessContact:
+        for i in range(0, how_many):
+            card_list.append(card_list)
+            card_list[i] = BusinessContact(
+                first_name=fake.first_name(),
+                surname=fake.last_name(),
+                address=fake.address(),
+                email=fake.email(),
+                work_phone=fake.phone_number(),
+                company=fake.company(),
+                job=fake.job(),
+            )
+            card_list[i].label_lenght = 0
+        return card_list
+    else:
+        exit(1)
 
 
 if __name__ == "__main__":
@@ -54,7 +109,7 @@ if __name__ == "__main__":
 
     print(karta)
     print(karta.label_lenght)
-    karty = create_contacts(5)
+    karty = create_contacts(BaseContact, 5)
     for i in karty:
         print(i)
 
@@ -68,3 +123,10 @@ if __name__ == "__main__":
     print("Wizytówki posortowane po imieniu:", by_first_name)
     print("Wizytówki posortowane po nazwisku:", by_surname)
     print("Wizytówki posortowane po e-mail:", by_email)
+
+    # karta.label_lenght = 5
+    print(karta.label_lenght)
+
+    karty[3].contact()
+    create_contacts(BusinessContact, 5)[2].contact()
+    create_contacts(BaseContact, 5)[1].contact()
